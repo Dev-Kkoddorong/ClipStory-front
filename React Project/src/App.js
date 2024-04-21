2abaf248827aac4b22033745493193d54e517aba
minkui1826 <minkui1826@kyonggi.ac.kr>
2024-04-22 01:59:19 +0900

    fix: 영화 목록 페이징으로 구현 #4

--- React Project/src/App.js
+++ React Project/src/App.js
@@ -1,20 +1,50 @@
+import React, { useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import './App.css';
 
+const postsPerPage = 10;
+const allPosts = Array(100).fill().map((_, i) => `글 ${i + 1}`);
+
 function App() {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handlePageClick = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
+  const currentPosts = allPosts.slice(
+    currentPage * postsPerPage,
+    (currentPage + 1) * postsPerPage
+  );
+
   return (
     <div>
-      <title>영화 추천 사이트</title>
+        <title>영화 추천 사이트</title>
         <header>
             <h1>영화 추천 사이트</h1>
         </header>
-        <div>
-          <MyListComponent num='10'/>
-        </div>
+      <ul className='container'>
+        {currentPosts.map((post, index) => (
+          <MovieComponent />
+        ))}
+      </ul>
+      <ReactPaginate
+        previousLabel={'이전'}
+        nextLabel={'다음'}
+        breakLabel={'...'}
+        pageCount={Math.ceil(allPosts.length / postsPerPage)}
+        marginPagesDisplayed={10}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={'pagination'}
+        subContainerClassName={'pages pagination'}
+        activeClassName={'active'}
+      />
     </div>
   );
 }
 
-let MovieComponent = ()=>{
+let MovieComponent = (props)=>{
   return (
   <div className="movie">
       <div className="movie-info">
@@ -25,21 +55,4 @@ let MovieComponent = ()=>{
   );
 }
 
-function MyListComponent(props) {
-  const dataList = [];
-  for(let i=0;i<props.num;i++) {
-    dataList.push({
-      id:i+1,
-      content: `내용 ${i+1}`
-    });
-  }
-  
-  return (
-    <div className="listContainer">
-      {dataList.map(data => (
-        <MovieComponent />
-      ))}
-    </div>
-  );
-}
 export default App;
