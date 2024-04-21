d87766fcbae00b24217ec33d5c45a1d3f2f3e88e
aldic9905 <61223424+aldic9905@users.noreply.github.com>
2024-04-21 14:27:04 +0900

    feat: 영화 목록 컴포넌트 추가 #4

--- React Project/src/App.js
+++ React Project/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <title>영화 추천 사이트</title>
+        <header>
+            <h1>영화 추천 사이트</h1>
+        </header>
+        <div>
+          <MyListComponent num='10'/>
+        </div>
     </div>
   );
 }
 
+let MovieComponent = ()=>{
+  return (
+  <div className="movie">
+      <div className="movie-info">
+          <h3>영화 제목</h3>
+          <p>영화 설명...</p>
+      </div>
+  </div>
+  );
+}
+
+function MyListComponent(props) {
+  const dataList = [];
+  for(let i=0;i<props.num;i++) {
+    dataList.push({
+      id:i+1,
+      content: `내용 ${i+1}`
+    });
+  }
+  
+  return (
+    <div className="listContainer">
+      {dataList.map(data => (
+        <MovieComponent />
+      ))}
+    </div>
+  );
+}
 export default App;
