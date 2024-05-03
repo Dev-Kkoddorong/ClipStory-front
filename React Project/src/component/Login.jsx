664d26dfd40a9b2b12e1471a991813b439b7443b
naega-leejaewon <woqmffl2000@naver.com>
2024-05-03 23:08:14 +0900

    feat: 로그인 페이지 구현 #12

---
+++ React Project/src/component/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return (
+        <div className="login-container">
+            <h2>Clip story 로그인</h2>
+            <form action="/login" method="post">
+                <input type="text" name="username" placeholder="아이디" required /><br />
+                <input type="password" name="password" placeholder="비밀번호" required /><br />
+                <input type="submit" value="로그인" />
+            </form>
+            <div className="signup-link">
+                <p>아이디가 없으신가요?</p>
+                <a href="/signup">회원가입</a>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
