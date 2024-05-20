1faa121df92b81d3c2278fba065d036b81061b7a
naega-leejaewon <woqmffl2000@naver.com>
2024-05-20 13:23:17 +0900

    HTML&CSS: 추천 페이지 보완#28

--- React Project/src/component/MovieSuggestion.js
+++ React Project/src/component/MovieSuggestion.js
@@ -80,6 +80,34 @@ function MovieSuggestion() {
           </div>
         </div>
         <div>
+          Clip Story가 회원님과 취향이 비슷한 사람들의 영화를 추천해 보았어요
+          <div id="recomment_again">
+            <Link to="/select1">
+              <button id="again">다시</button>
+            </Link>
+          </div>
+        </div>
+        <div className="suggestionContainer">
+          <div className="screen">
+            <h2>{MovieList[0].id}</h2>
+            <h2>{MovieList[0].title}</h2>
+            <h2>{MovieList[0].genreNameList}</h2>
+            <p>This is the content of screen 1.</p>
+          </div>
+          <div className="screen">
+            <h2>{MovieList[1].id}</h2>
+            <h2>{MovieList[1].title}</h2>
+            <h2>{MovieList[1].genreNameList}</h2>
+            <p>This is the content of screen 2.</p>
+          </div>
+          <div className="screen">
+            <h2>{MovieList[2].id}</h2>
+            <h2>{MovieList[2].title}</h2>
+            <h2>{MovieList[2].genreNameList}</h2>
+            <p>This is the content of screen 3.</p>
+          </div>
+        </div>
+        <div>
           <Link to="/">
             <button className="box">목록 보기</button>
           </Link>
